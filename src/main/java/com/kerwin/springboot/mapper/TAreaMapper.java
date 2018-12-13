0cbddaee4aca90dff5f32de225638472f11ec60a
kerwin-wang <wdzdh09@163.com>
2018-12-13 13:07:12 +0800

    添加@component注解给dao

--- src/main/java/com/kerwin/springboot/mapper/TAreaMapper.java
+++ src/main/java/com/kerwin/springboot/mapper/TAreaMapper.java
@@ -2,6 +2,8 @@ package com.kerwin.springboot.mapper;
 
 import com.kerwin.springboot.entity.TArea;
 import com.kerwin.springboot.util.MyMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface TAreaMapper extends MyMapper<TArea> {
 }
--- src/main/java/com/kerwin/springboot/mapper/UsersMapper.java
+++ src/main/java/com/kerwin/springboot/mapper/UsersMapper.java
@@ -2,6 +2,8 @@ package com.kerwin.springboot.mapper;
 
 import com.kerwin.springboot.entity.Users;
 import com.kerwin.springboot.util.MyMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UsersMapper extends MyMapper<Users> {
 }
